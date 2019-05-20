c0d53bed105b651181a1e3055ab6e847f1cfc2c1
Jasmine Carter <jasmine.carter87@gmail.com>
2019-05-20 23:59:48 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,8 +2,8 @@
 def square_array(array)
   # your code here
 array.each do |element|
-squared_array = []
+  squared_array = []
   squared_element = element ** 2
-  squared_array.unshift(squared_element)
+  squared_array.push(squared_element)
 end
 end
