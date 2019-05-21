c552c09ae84487e0a17548c4546acda58e11dd84
Jasmine Carter <jasmine.carter87@gmail.com>
2019-05-21 00:08:16 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -4,6 +4,6 @@ squared_array = []
   array.each do |element|
     squared_element = element ** 2
     squared_array.push(squared_element)
-    return squared_array
   end
+  return squared_array
 end
